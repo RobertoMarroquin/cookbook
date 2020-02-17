e608e3f454f2bb61b1d35a10d72bd1763c424240
Roberto Marroquin <robma96@gmail.com>
2020-02-17 09:55:05 -0600

    collect static

--- coockbook/settings.py
+++ coockbook/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR,'/static/')
 
-STATIC_ROOT = '/static/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/staticfiles/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
