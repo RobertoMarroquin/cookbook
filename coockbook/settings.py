32a39d0c47f401ab6d89767b45d176e24cc78727
Roberto Marroquin <robma96@gmail.com>
2020-02-17 10:43:01 -0600

    CollectStatic

--- coockbook/settings.py
+++ coockbook/settings.py
@@ -121,12 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_URL = os.path.join(BASE_DIR,'/static/')
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR,'/staticfiles/')
-
-os.makedirs(STATIC_URL, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
