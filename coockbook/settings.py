5f2a0fe63220baae39c75db633fd1cec8d56e8fc
Roberto Marroquin <robma96@gmail.com>
2020-02-17 09:58:40 -0600

    staticfiles

--- coockbook/settings.py
+++ coockbook/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'coockbook.urls'
@@ -121,8 +122,11 @@ USE_TZ = True
 
 STATIC_URL = os.path.join(BASE_DIR,'/static/')
 
-STATIC_ROOT = os.path.join(BASE_DIR,'/static/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR,'/staticfiles/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
